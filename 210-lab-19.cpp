b29a119f981f8909b519feb7032502f690022640
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-10-04 15:23:45 -0700

    created Movie class

--- 210-lab-19.cpp
+++ 210-lab-19.cpp
@@ -2,8 +2,30 @@
 // IDE used: Visual Studio Code
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std; 
 
+class Movie {
+private: 
+    struct Node {
+        float rating; 
+        Node *next;
+    };
+    string title;
+    Node* head;
+    string comments; 
+public:
+    Node* addToHead(Node* hd, float rating) { 
+    Node* newNode = new Node; 
+    newNode->rating = rating; 
+    newNode->next = hd; 
+    return newNode;
+}
+};
+
 int main() {
+    vector<Movie> moveVector;
+
     return 0; 
 }
+
