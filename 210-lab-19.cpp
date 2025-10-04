14b01d1110998bb5565d9cb302b5c29521c1e027
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-10-04 15:36:34 -0700

    added addReviewsToHead() and prunt()

--- 210-lab-19.cpp
+++ 210-lab-19.cpp
@@ -15,21 +15,52 @@ private:
     Node* head;
     string comments; 
 public:
+    Movie() {
+        title = "";
+        comments = ""; 
+        head = nullptr; 
+    }
+
     void setTitle(string t) { title = t; }
     void setComments(string c) {comments = c; }
     string getTitle() { return title; } 
     string getComments() { return comments; }
-    Node* addToHead(Node* hd, float rating) { 
+    void addReviewToHead(float rating) { 
         Node* newNode = new Node; 
         newNode->rating = rating; 
-        newNode->next = hd; 
-        return newNode;
+        newNode->next = head;
+        head = newNode;
+    }
+
+    void printReviews() { 
+        if (!head) {
+        cout << "Empty list.\n";
+        return;
+        }
+        Node* current = head;
+        int reviewNum = 0; 
+        float totalRatingScore = 0.0; 
+        while (current) {
+            cout << "    > Review #" << reviewNum++ << ": " << current->rating << endl; 
+            totalRatingScore += current->rating; 
+            current = current->next;
+        }
+        cout << "    > Average: " << totalRatingScore/reviewNum;
+        cout << endl;
     }
 };
 
 int main() {
-    vector<Movie> moveVector;
+    vector<Movie> movies;
 
+    Movie movie1; 
+    movie1.setTitle("hell world");
+    movie1.setComments("amazing");
+    movie1.addReviewToHead(2.3);
+    movie1.addReviewToHead(1.3);
+    movie1.addReviewToHead(4.5);
+
+    movie1.printReviews(); 
     return 0; 
 }
 
